5e64db2255ab8458bf31d75568e9f65421d8ca09
fandy <ahmad.ariffandy@gmail.com>
2023-09-20 12:02:54 +0700

    img url to webserver

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -1,7 +1,7 @@
 <tbody>
     <tr>
         <td style="text-align: center; padding-bottom:25px">
-            <a href="#"><img style="height: 60px" src="http://dev.ifca.co.id:8080/sendmailapi/public/images/header.png" alt="logo"></a>
+            <a href="#"><img style="height: 60px" src="{{ url('/public/images/header.png') }}" alt="logo"></a>
             <p style="font-size: 16px; color: #026735; padding-top: 0px;">PT. Suryacipta Swadaya</p>
            
         </td>
