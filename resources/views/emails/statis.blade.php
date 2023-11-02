6bf0ec0512da44a7aa66988f351143765b3585d6
fandy <ahmad.ariffandy@gmail.com>
2023-11-02 17:59:32 +0700

    Add table to RevenueSharing MailBody

--- resources/views/emails/statis.blade.php
+++ resources/views/emails/statis.blade.php
@@ -8,31 +8,13 @@
     <title></title>
     
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,600" rel="stylesheet" type="text/css">
-    <!-- Web Font / @font-face : BEGIN -->
-    <!--[if mso]>
-        <style>
-            * {
-                font-family: 'Roboto', sans-serif !important;
-            }
-        </style>
-    <![endif]-->
-
-    <!--[if !mso]>
-        <link href="https://fonts.googleapis.com/css?family=Roboto:400,600" rel="stylesheet" type="text/css">
-    <![endif]-->
-
-    <!-- Web Font / @font-face : END -->
-
-    <!-- CSS Reset : BEGIN -->
-    
-    
     @include('template.style')
 
 </head>
 
 <body width="100%" style="margin: 0; padding: 0 !important; mso-line-height-rule: exactly; background-color: #e6f0eb;">
 	<div style="width: 100%; background-color: #e6f0eb;">
-        <table width="50%" border="0" cellpadding="0" cellspacing="0" bgcolor="#f5f0ea" style="margin-left: auto;margin-right: auto;" >
+        <table width="50%" border="0" cellpadding="0" cellspacing="0" bgcolor="#e6f0eb" style="margin-left: auto;margin-right: auto;" >
             <tr>
                <td style="padding: 40px 0;">
                     <table style="width:100%;max-width:620px;margin:0 auto;">
@@ -41,8 +23,56 @@
                     <table style="width:100%;max-width:620px;margin:0 auto;background-color:#ffffff;">
                         <tbody>
                             <tr>
-                                <td style="text-align:center;padding: 50px 30px;">
-                                    <img style="width:88px; margin-bottom:24px;" src="{{ url('/public/images/') }}/<?php echo $image ?>" alt="Verified">
+                                <td style="text-align:center;padding: 30px 30px 20px">
+                                    <h5 style="text-align:left;margin-bottom: 24px; color: #526484; font-size: 20px; font-weight: 400; line-height: 28px;">Dear data['user_name'] </h5>
+                                    <p style="text-align:left;margin-bottom: 15px; color: #526484; font-size: 16px;">Please Approve Revenue Sharing with details :</p>
+                                    <table style="padding-left: 40px;width: 100%; text-align:left;">
+                                        <tr>
+                                            <td style="width: 120px;">Document No. </td>
+                                            <td>:</td>
+                                            <td>data['doc_no'] </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Customer Name</td>
+                                            <td>:</td>
+                                            <td>data['Name'] </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Currency Code</td>
+                                            <td>:</td>
+                                            <td>IDR </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Total Sales</td>
+                                            <td>:</td>
+                                            <td>000.000.000 </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Tarif (%)</td>
+                                            <td>:</td>
+                                            <td>0.00 </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Base Amount</td>
+                                            <td>:</td>
+                                            <td>000.000.000 </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Tax Amount</td>
+                                            <td>:</td>
+                                            <td>000.000.000 </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Total Amount</td>
+                                            <td>:</td>
+                                            <td>000.000.000 </td>
+                                        </tr>
+                                    </table>
+                                    <br>
+                                    <a href="" style="background-color:#1ee0ac;border-radius:4px;color:#ffffff;display:inline-block;font-size:13px;font-weight:600;line-height:44px;text-align:center;text-decoration:none;text-transform: uppercase; padding: 0px 40px;margin: 10px">Approve</a>
+                                    <a href="" style="background-color:#f4bd0e;border-radius:4px;color:#ffffff;display:inline-block;font-size:13px;font-weight:600;line-height:44px;text-align:center;text-decoration:none;text-transform: uppercase; padding: 0px 40px;margin: 10px">Revise</a>
+                                    <a href="" style="background-color:#e85347;border-radius:4px;color:#ffffff;display:inline-block;font-size:13px;font-weight:600;line-height:44px;text-align:center;text-decoration:none;text-transform: uppercase; padding: 0px 40px;margin: 10px">Cancel</a>
+                                    <br>
                                 </td>
                             </tr>
                         </tbody>
