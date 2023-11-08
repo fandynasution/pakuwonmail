116cf12c07a18a2e5812d30236fa9537c928048d
fandy <ahmad.ariffandy@gmail.com>
2023-11-08 17:06:52 +0700

    Red Box

--- resources/views/emails/landapproval/send.blade.php
+++ resources/views/emails/landapproval/send.blade.php
@@ -53,7 +53,7 @@
                                         <tr>
                                             <td>Luas Tanah Gambar Ukur</td>
                                             <td>:</td>
-                                            <td>{{ $data['land_area'] }}</td>
+                                            <td>{{ $data['land_area'] }} M&sup2</td>
                                         </tr>
                                     </table>
                                     <br>
