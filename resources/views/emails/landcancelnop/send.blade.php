5a6add76b92f10c8db6108bd52853b83f74bec86
fandy <ahmad.ariffandy@gmail.com>
2024-01-10 09:40:33 +0700

    NOP Cancel Display

--- resources/views/emails/landcancelnop/send.blade.php
+++ resources/views/emails/landcancelnop/send.blade.php
@@ -28,27 +28,27 @@
                     <table style="width:100%;max-width:600px;margin:0 auto;">
                         @include('template.header')
                     </table>
-                    <table style="margin-left:200px;width:100%;max-width:800px;margin:0 auto;background-color:#ffffff;">
+                    <table style="margin: 0 auto; width: 100%; max-width: 800px; background-color: #ffffff;">
                         <tbody>
                             <tr>
-                                <td style="text-align:center;padding: 0px 30px 0px 20px">
-                                    <h5 style="text-align:left;color: #526484; font-size: 20px; font-weight: 400; line-height: 28px;">Untuk Bapak/Ibu {{ $dataArray['user_name'] }}</h5>
-                                    <p style="text-align:left;color: #526484; font-size: 16px;">Tolong berikan persetujuan untuk proses Pembatalan NOP dengan detail :</p>
-                                    <table cellpadding="0" cellspacing="0" style="text-align:left;width:100%;max-width:800px;margin:0 auto;background-color:#ffffff; ">
+                                <td style="text-align: center; padding: 20px;">
+                                    <h5 style="color: #526484; font-size: 20px; font-weight: 400; line-height: 28px; text-align: left;">Untuk Bapak/Ibu {{ $dataArray['user_name'] }}</h5>
+                                    <p style="color: #526484; font-size: 16px; text-align: left;">Tolong berikan persetujuan untuk proses Pembatalan NOP dengan detail :</p>
+                                    <table cellpadding="0" cellspacing="0" style="width: 100%; max-width: 800px; background-color: #ffffff;">
                                         <tr>
-                                            <td>Nomor Dokumen </td>
-                                            <td> : </td>
-                                            <td> {{ $dataArray['doc_no'] }} </td>
+                                            <td>Nomor Dokumen</td>
+                                            <td>:</td>
+                                            <td>{{ $dataArray['doc_no'] }}</td>
                                         </tr>
                                         <tr>
-                                            <td>NOP </td>
-                                            <td> : </td>
-                                            <td> {{ $dataArray['nop_no'] }} </td>
+                                            <td>NOP</td>
+                                            <td>:</td>
+                                            <td>{{ $dataArray['nop_no'] }}</td>
                                         </tr>
                                         <tr>
                                             <td>Nama pada SPPT</td>
-                                            <td> : </td>
-                                            <td> {{ $dataArray['sppt_name'] }} </td>
+                                            <td>:</td>
+                                            <td>{{ $dataArray['sppt_name'] }}</td>
                                         </tr>
                                         <tr>
                                             <td>Nama Pemilik</td>
@@ -62,7 +62,7 @@
                                         </tr>
                                     </table>
                                     <br>
-                                    <p style="text-align:left;margin-bottom: 15px; color: #000000; font-size: 16px;">
+                                    <p style="margin-bottom: 15px; color: #000000; font-size: 16px; text-align: left;">
                                         <b>Terima Kasih,</b><br>
                                         {{ $dataArray['sender_name'] }}
                                     </p>
