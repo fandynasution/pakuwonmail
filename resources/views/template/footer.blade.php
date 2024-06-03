304d4e301bb875c1e89ee386108afff3881857f4
ahmadfandy <ahmad.ariffandy@gmail.com>
2024-06-03 10:56:38 +0700

    Footer

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -1,7 +1,7 @@
 <tbody>
     <tr>
         <td style="text-align: center; padding:25px 20px 0;">
-            <p style="font-size: 13px;">Copyright © 2023 IFCA Software. All rights reserved.</p>
+            <p style="font-size: 13px;">Copyright © 2023 STARPMS. All rights reserved.</p>
         </td>
     </tr>
 </tbody>
