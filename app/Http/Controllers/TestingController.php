009c82aa91bd7dc2d5d47155eef61598dab404a4
ahmadfandy <ahmad.ariffandy@gmail.com>
2024-06-04 12:48:52 +0700

    aa

--- app/Http/Controllers/TestingController.php
+++ app/Http/Controllers/TestingController.php
@@ -18,7 +18,7 @@ class TestingController extends Controller
         $first = 'PO/JSU/23/88888';
         $new_ref_no = str_replace("/","-",$first);
         echo $new_ref_no;
-        var_dump("aaa");
+        var_dump("bbb");
     }
 
     public function gambar()
