0d70d0a455c6ca1a8c567bf878557b7fc9c433fe
ahmadfandy <ahmad.ariffandy@gmail.com>
2024-06-20 14:45:58 +0700

    Carbon

--- app/Http/Controllers/LandApprovalController.php
+++ app/Http/Controllers/LandApprovalController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Log;
 use App\Mail\LandApprovalMail;
 use App\Mail\UserEmail;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class LandApprovalController extends Controller
@@ -74,31 +75,20 @@ class LandApprovalController extends Controller
 
     public function changestatus($status='', $entity_cd='', $doc_no='', $level_no='')
     {
-        $where2 = array(
+        $where = array(
             'doc_no'        => $doc_no,
-            'status'        => array("A",'R', 'C'),
             'entity_cd'     => $entity_cd,
             'level_no'      => $level_no,
             'type'          => 'B',
             'module'        => 'LM',
         );
 
-        $where3 = array(
-            'doc_no'        => $doc_no,
-            'entity_cd'     => $entity_cd,
-            'level_no'      => $level_no,
-            'type'          => 'B',
-            'module'        => 'LM',
-        );
         $query = DB::connection('SSI')
         ->table('mgr.cb_cash_request_appr')
-        ->where($where2)
+        ->where($where)
+        ->whereIn('status', ["A", "R", "C"])
         ->get();
 
-        $query3 = DB::connection('SSI')
-        ->table('mgr.cb_cash_request_appr')
-        ->where($where3)
-        ->get();
         if(count($query)>0){
             $msg = 'You Have Already Made a Request to MAP Land Doc. No. '.$doc_no ;
             $notif = 'Restricted !';
@@ -109,32 +99,61 @@ class LandApprovalController extends Controller
                 "St" => $st,
                 "notif" => $notif,
                 "image" => $image
-            );return view("emails.after", $msg1);
-        } else {
-            if ($status == 'A') {
-                $name   = 'Approval';
-                $bgcolor = '#40de1d';
-                $valuebt  = 'Approve';
-            }else if ($status == 'R') {
-                $name   = 'Revision';
-                $bgcolor = '#f4bd0e';
-                $valuebt  = 'Revise';
-            } else if ($status == 'C'){
-                $name   = 'Cancelation';
-                $bgcolor = '#e85347';
-                $valuebt  = 'Cancel';
-            }
-            $data = array(
-                'entity_cd'     => $entity_cd, 
-                'doc_no'        => $doc_no, 
-                'status'        => $status,
-                'level_no'      => $level_no, 
-                'name'          => $name,
-                'bgcolor'       => $bgcolor,
-                'valuebt'       => $valuebt
             );
+            return view("emails.after", $msg1);
+        } else {
+            $where2 = array(
+                'doc_no'        => $doc_no,
+                'status'        => 'P',
+                'entity_cd'     => $entity_cd,
+                'level_no'      => $level_no,
+                'type'          => 'B',
+                'module'        => 'LM',
+            );
+
+            $query2 = DB::connection('SSI')
+            ->table('mgr.cb_cash_request_appr')
+            ->where($where2)
+            ->get();
+
+            if (count($query2) == 0) {
+                $msg = 'There is no MAP Land with Doc. No. '.$doc_no ;
+                $notif = 'Restricted !';
+                $st  = 'OK';
+                $image = "double_approve.png";
+                $msg1 = array(
+                    "Pesan" => $msg,
+                    "St" => $st,
+                    "notif" => $notif,
+                    "image" => $image
+                );
+                return view("emails.after", $msg1);
+            } else {
+                if ($status == 'A') {
+                    $name   = 'Approval';
+                    $bgcolor = '#40de1d';
+                    $valuebt  = 'Approve';
+                } else if ($status == 'R') {
+                    $name   = 'Revision';
+                    $bgcolor = '#f4bd0e';
+                    $valuebt  = 'Revise';
+                } else if ($status == 'C') {
+                    $name   = 'Cancelation';
+                    $bgcolor = '#e85347';
+                    $valuebt  = 'Cancel';
+                }
+                $data = array(
+                    'entity_cd'     => $entity_cd, 
+                    'doc_no'        => $doc_no, 
+                    'status'        => $status,
+                    'level_no'      => $level_no, 
+                    'name'          => $name,
+                    'bgcolor'       => $bgcolor,
+                    'valuebt'       => $valuebt
+                );
+                return view('emails/landapproval/action', $data);
+            }
         }
-        return view('emails/landapproval/action', $data);
     }
 
     public function update(Request $request)
