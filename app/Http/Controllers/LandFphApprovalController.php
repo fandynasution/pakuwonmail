86d8c60c2ad2258617ffa3360e91783f4d427db1
ahmadfandy <ahmad.ariffandy@gmail.com>
2024-07-15 10:58:24 +0700

    date

--- app/Http/Controllers/LandFphApprovalController.php
+++ app/Http/Controllers/LandFphApprovalController.php
@@ -31,7 +31,8 @@ class LandFphApprovalController extends Controller
         $total_amt = number_format($request->total_amt, 2, '.', ',');
         $book_amt = number_format($request->book_amt, 2, '.', ',');
 
-        $transaction_date = Carbon::createFromFormat('Y-m-d H:i:s.u', $request->transaction_date)->format('d-m-Y');
+        $transaction_date = Carbon::createFromFormat('M  j Y h:iA', $request->transaction_date)->format('d-m-Y');
+        // Carbon::createFromFormat('Y-m-d H:i:s.u', $request->transaction_date)->format('d-m-Y');
 
         $dataArray = array(
             'user_id'       => $request->user_id,
