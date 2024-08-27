de0d4f80eba1f1c53a647feea9e5c3fbd52d12f5
ahmadfandy <ahmad.ariffandy@gmail.com>
2024-08-27 23:10:54 +0700

    Verification & Measuring

--- app/Http/Controllers/LandMeasuringController.php
+++ app/Http/Controllers/LandMeasuringController.php
@@ -28,8 +28,7 @@ class LandMeasuringController extends Controller
         {
             $link[] = $show;
         }
-
-        $transaction_date = Carbon::createFromFormat('Y-m-d H:i:s.u', $request->transaction_date)->format('d-m-Y');
+        $transaction_date = Carbon::createFromFormat('M  j Y h:iA', $request->transaction_date)->format('d-m-Y');
 
         $dataArray = array(
             'user_id'       => $request->user_id,
