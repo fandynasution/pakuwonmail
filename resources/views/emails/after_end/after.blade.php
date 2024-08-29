b85c44760a429a0e24a146957935cb746ceaf92d
Ahmad Ariffandy Nasution <ahmad.ariffandy@ifca.co.id>
2024-08-29 14:09:01 +0700

    Boundary Entity

--- resources/views/emails/after_end/after.blade.php
+++ resources/views/emails/after_end/after.blade.php
@@ -36,11 +36,20 @@
             <tr>
                <td style="padding: 40px 0;">
                     <table style="width:100%;max-width:620px;margin:0 auto;">
-                        @empty($data['entity_name'])
-                            PT. Suryacipta Swadaya
-                        @else
-                            {{ $data['entity_name'] }}
-                        @endempty
+                    <tbody>
+                        <tr>
+                            <td style="text-align: center; padding-bottom:25px;color: #000000 !important;">
+                                <img width = "120" src="{{ url('/public/images/header.png') }}" alt="logo">
+                                <p style="font-size: 16px; color: #000000; padding-top: 0px;">
+                                    @empty($entity_name)
+                                        PT. Suryacipta Swadaya
+                                    @else
+                                        {{ $entity_name }}
+                                    @endempty
+                                </p>
+                            </td>
+                        </tr>
+                    </tbody>
                     </table>
                     <table style="width:100%;max-width:620px;margin:0 auto;background-color:#ffffff;font-size: 14px; color: #000000;">
                         <tbody>
