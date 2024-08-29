b571badfa11a75178cef007ba2eed4a5cc0baee7
Ahmad Ariffandy Nasution <ahmad.ariffandy@ifca.co.id>
2024-08-29 14:27:25 +0700

    Handover SHGB Entity

--- resources/views/emails/landhandovershgb/action.blade.php
+++ resources/views/emails/landhandovershgb/action.blade.php
@@ -22,7 +22,11 @@
                         <td style="text-align: center; padding-bottom:25px;color: #000000 !important;">
                             <img width = "120" src="{{ url('/public/images/header.png') }}" alt="logo">
                             <p style="font-size: 16px; color: #000000; padding-top: 0px;">
-                            111
+                                @empty(trim($data['entity_name']))
+                                    PT. Suryacipta Swadaya
+                                @else
+                                    {{ trim($data['entity_name']) }}
+                                @endempty
                             </p>
                         </td>
                     </tr>
